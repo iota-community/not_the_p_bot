361549326f7d9224142f4ffd34106fd8d7299437
Antonio Nardella <antonio.nardella@iota.org>
2022-04-06 16:27:43 +0200

    added rounding

--- notthepbot.py
+++ notthepbot.py
@@ -1,8 +1,6 @@
 import discord
 import requests
 import json
-import re
-
 
 coin_id = "iota"
 url_coin = f"https://api.coingecko.com/api/v3/coins/{coin_id}"
@@ -20,15 +18,15 @@ class MyClient(discord.Client):
             response = requests.get(url_coin)
             iota_prices = json.loads(response.text)
             marketcaprank = iota_prices["market_cap_rank"]
-            currentprice = iota_prices["market_data"]["current_price"]["usd"]
-            change24hours = iota_prices["market_data"]["price_change_percentage_24h"]
-            change1hour = iota_prices["market_data"]["price_change_percentage_24h"]/24
+            currentprice = round(iota_prices["market_data"]["current_price"]["usd"], 4)
+            change24hours = round(iota_prices["market_data"]["price_change_percentage_24h"], 1)
+            change1hour = round(iota_prices["market_data"]["price_change_percentage_24h"] / 24, 1)
             #print (iota_prices["market_cap_rank"])
             embedVar=discord.Embed(title = str(iota_prices["name"]) + " #" + str(marketcaprank))
             embedVar=discord.Embed(color = 0xff7800)
-            embedVar.add_field(name="USD", value=str(currentprice)[0:6], inline=True)
-            embedVar.add_field(name="1h", value=str(change1hour)[0:4]+"%", inline=True)
-            embedVar.add_field(name="24h", value=str(change24hours)[0:4]+"%", inline=True)
+            embedVar.add_field(name="USD", value=str(currentprice), inline=True)
+            embedVar.add_field(name="1h", value=str(change1hour) + "%", inline=True)
+            embedVar.add_field(name="24h", value=str(change24hours) + "%", inline=True)
             embedVar.add_field(name="Source", value="Coingecko", inline=False)
             #print (embedVar)
             #await message.channel.send(iota_prices["market_cap_rank"])
