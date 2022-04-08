85e4ca84c0514cd453e029298945bd2d5c813fe7
Antonio Nardella <antonio.nardella@iota.org>
2022-04-08 13:30:01 +0200

    added comments

--- notthepbot.py
+++ notthepbot.py
@@ -13,15 +13,24 @@ class MyClient(discord.Client):
         # don't respond to ourselves
         if message.author == self.user:
             return
+
+        # list of inputs/commands it should listen to
         speccommands = ['p', 'price']
+        # let's read the message 
         if message.content.casefold() in str(speccommands).casefold():
+            # request response from Coingecko API
             response = requests.get(url_coin)
+            
+            # load the json to extract the data we are looking for
             iota_prices = json.loads(response.text)
+
+            # fill variables
             marketcaprank = iota_prices["market_cap_rank"]
             currentprice = round(iota_prices["market_data"]["current_price"]["usd"], 4)
             change24hours = round(iota_prices["market_data"]["price_change_percentage_24h"], 1)
             change1hour = round(iota_prices["market_data"]["price_change_percentage_1h_in_currency"]["usd"], 1)
             
+            # change color of the embed based on the value of the change1hour variable
             if change1hour >= 0:
                 if change1hour == 0:
                     embedcolor = 0xff7800
@@ -30,14 +39,17 @@ class MyClient(discord.Client):
             else:
                 embedcolor = 0xe01b24
             
+            # build the embed message
             embedVar=discord.Embed(title =  str(iota_prices["name"]) + " #" + str(marketcaprank), color = embedcolor)
             embedVar.add_field(name="USD", value=str(currentprice), inline=True)
             embedVar.add_field(name="1h", value=str(change1hour) + "$", inline=True)
             embedVar.add_field(name="24h", value=str(change24hours) + "%", inline=True)
             embedVar.add_field(name="Source", value="Coingecko", inline=False)
 
+            # reply to the input/command with the embed
             await message.channel.send(embed=embedVar)
 
+# load discord intents
 intents = discord.Intents.default()
 intents.messages = True
 client = MyClient(intents=intents)
